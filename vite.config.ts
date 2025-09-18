4757b2d4dca7b9afd6179ec927d9a15a69096cdc
Suraj Kumar <surajkumar06174@gmail.com>
2025-09-18 16:02:34 +0530

    Set mode-based base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  base: "/TechLifto/",
+  base: mode === "production" ? "/TechLifto/" : "/",
   server: {
     host: "::",
     port: 8080,
